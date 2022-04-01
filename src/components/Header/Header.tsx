d2de644f6052c6b6f0116c2982c62caafde45881
Kirito <kurulluvarov@gmail.com>
2022-04-01 23:40:58 +0300

    firebase in index,headerC

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+// import {AppBar,MenuIcon,IconButton,Toolbar,Box } from 'material-ui/core/';
+// import Typography from '@material-ui/core/Typography';
+export  const Header = () => {
+  return <div>
+    
+    
+      Header
+    </div>
+}
+// <Box sx={{ flexGrow: 1 }}>
+    //   <AppBar position="static">
+    //     <Toolbar variant="dense">
+    //       <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+    //         <MenuIcon />
+    //       </IconButton>
+    //       <p>
+    //         News
+    //       </p>
+    //     </Toolbar>
+    //   </AppBar>
+    // </Box>
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import '@firebase/firestore'
+import 'firebase/auth'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxzsI4wEjdQ_DGIe239-AQVmtjKDKxW4U",
+  authDomain: "sport-shop-fd1bd.firebaseapp.com",
+  projectId: "sport-shop-fd1bd",
+  storageBucket: "sport-shop-fd1bd.appspot.com",
+  messagingSenderId: "688170118195",
+  appId: "1:688170118195:web:d4ccba5bdd36ee67f4c746"
+};
+
+const app = initializeApp(firebaseConfig);
+
+
+const auth = getAuth(app)
+console.log(auth)
 
 ReactDOM.render(
+ 
   <React.StrictMode>
     <App />
+    <button onClick={()=>signInWithPopup(auth,new GoogleAuthProvider())}></button>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
