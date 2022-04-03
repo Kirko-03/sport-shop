7a2f6c23ee2d686787f750d9a2e72f9d0117f4f6
Kirito <kurulluvarov@gmail.com>
2022-04-03 21:46:20 +0300

    materialui

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,39 @@
-// import {AppBar,MenuIcon,IconButton,Toolbar,Box } from 'material-ui/core/';
-// import Typography from '@material-ui/core/Typography';
+import {AppBar,Button,Paper,MenuList,Typography,Box, IconButton, LinearProgress,MenuItem, Toolbar} from '@material-ui/core'
+import {Menu} from '@material-ui/icons'
+import  { useState } from 'react';
+
 export  const Header = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return <div>
-    
-    
-      Header
+ <Box sx={{ flexGrow: 1 }}>
+   <AppBar position="static">
+     <Toolbar variant="dense">
+     <IconButton style={{color:'cyan'}} aria-label="upload picture" component="span">
+         
+        {open?<div onBlur={()=>handleClick()}>
+          <Menu onClick={()=>handleClick()} />
+        <Paper style={{position:'absolute'}} >
+          
+        <MenuList>
+          <MenuItem>Profile</MenuItem>
+          <MenuItem>My account</MenuItem>
+          <MenuItem>Logout</MenuItem>
+        </MenuList>
+      </Paper></div>:<Menu onClick={()=>handleClick()} /> 
+          }
+          
+       </IconButton>
+       <Typography>
+        News         
+       </Typography>
+     </Toolbar>
+   </AppBar>
+   {/* <LinearProgress variant="query"/> */}
+ </Box>    
     </div>
 }
-// <Box sx={{ flexGrow: 1 }}>
-    //   <AppBar position="static">
-    //     <Toolbar variant="dense">
-    //       <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-    //         <MenuIcon />
-    //       </IconButton>
-    //       <p>
-    //         News
-    //       </p>
-    //     </Toolbar>
-    //   </AppBar>
-    // </Box>
