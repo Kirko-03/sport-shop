34e8e7c20d25c2942aa84a322192271be278040b
Kirito <kurulluvarov@gmail.com>
2022-04-04 22:25:14 +0300

    headerButton

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,26 @@ import {AppBar,Button,Paper,MenuList,Typography,Box, IconButton, LinearProgress,
 import {Menu} from '@material-ui/icons'
 import  { useState } from 'react';
 
+import '@firebase/firestore'
+import 'firebase/auth'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxzsI4wEjdQ_DGIe239-AQVmtjKDKxW4U",
+  authDomain: "sport-shop-fd1bd.firebaseapp.com",
+  projectId: "sport-shop-fd1bd",
+  storageBucket: "sport-shop-fd1bd.appspot.com",
+  messagingSenderId: "688170118195",
+  appId: "1:688170118195:web:d4ccba5bdd36ee67f4c746"
+};
+
+const app = initializeApp(firebaseConfig);
+
+
+const auth = getAuth(app)
+console.log(auth)
+
 export  const Header = () => {
   const [open, setOpen] = useState<boolean>(false);
 
@@ -28,10 +48,17 @@ export  const Header = () => {
           }
           
        </IconButton>
+       <div style={{display:'flex',flexDirection:'row',justifyContent:'space-between',width:'100em'}}>
        <Typography>
         News         
        </Typography>
+       <Typography>
+     <button onClick={()=>signInWithPopup(auth,new GoogleAuthProvider())}>Войти</button>
+       </Typography>
+       </div>
+
      </Toolbar>
+  
    </AppBar>
    {/* <LinearProgress variant="query"/> */}
  </Box>    
--- src/index.tsx
+++ src/index.tsx
@@ -3,31 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import App from './App';
-import { initializeApp } from "firebase/app";
-import '@firebase/firestore'
-import 'firebase/auth'
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDxzsI4wEjdQ_DGIe239-AQVmtjKDKxW4U",
-  authDomain: "sport-shop-fd1bd.firebaseapp.com",
-  projectId: "sport-shop-fd1bd",
-  storageBucket: "sport-shop-fd1bd.appspot.com",
-  messagingSenderId: "688170118195",
-  appId: "1:688170118195:web:d4ccba5bdd36ee67f4c746"
-};
-
-const app = initializeApp(firebaseConfig);
-
-
-const auth = getAuth(app)
-console.log(auth)
 
 ReactDOM.render(
  
   <React.StrictMode>
     <App />
-    <button onClick={()=>signInWithPopup(auth,new GoogleAuthProvider())}></button>
-  </React.StrictMode>,
+   </React.StrictMode>,
   document.getElementById('root')
 );
